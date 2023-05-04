c5249e1549feeab8f078cd8787fc698c0957d03d
Diogo Yaguas <diogo.yaguas@jumpseller.com>
2023-05-04 20:40:09 +0100

    Fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,11 @@
 import PropTypes from "prop-types"
 import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = ({ mode, setMode }) => {
 	return (
 		<nav class="navbar">
 			<div className="row w-100">
 				<div className="col-4">
-					<FontAwesomeIcon icon="fa-solid fa-bars" />
 				</div>
 				<div className="col-4 text-center">
 					<span>JOSE VAZ - CREATIVE SERVICES</span>
@@ -17,7 +15,6 @@ const Header = ({ mode, setMode }) => {
 						onClick={() => setMode(mode == "light" ? "dark" : "light")}
 						title={`Toggle ${mode === "light" ? "Dark" : "Light"} Mode`}
 					>	
-						<FontAwesomeIcon icon="fa-solid fa-circle-half-stroke" />
 					</span>
 				</div>
 			</div>
