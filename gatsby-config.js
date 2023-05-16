42dfd41414c8bf7a9bd408f7ad4c29d49148b314
Diogo Yaguas <diogo.yaguas@jumpseller.com>
2023-05-16 23:22:26 +0100

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        "icon": ""
+        "icon": "src/images/icon.png"
       }
     }, {
       resolve: 'gatsby-source-filesystem',
