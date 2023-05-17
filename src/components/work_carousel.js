bd0a528b3bc61fd4bacba4a5d4b2080394676661
Diogo Yaguas <diogo.yaguas@jumpseller.com>
2023-05-17 09:06:06 +0100

    Fix video overlay

--- src/components/work_carousel.js
+++ src/components/work_carousel.js
@@ -1,10 +1,11 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import React, { useRef, useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
 import Img from "gatsby-image"
-import React from "react"
+import { Link } from "gatsby"
 import ReactPlayer from 'react-player';
 import Slider from 'react-slick';
 
@@ -14,6 +15,7 @@ const WorkCarousel = () => {
       allWorkJson {
         nodes {
           name
+          link
           video {
             publicURL
           }
@@ -29,17 +31,35 @@ const WorkCarousel = () => {
     }
   `)
 
+    const [dragging, setDragging] = useState(false);
+    const sliderRef = useRef(null);
+
+    const handleDragStart = () => {
+        setDragging(true);
+    };
+
+    const handleDragEnd = () => {
+        setDragging(false);
+    };
+
     const settings = {
-        dots: false,
         arrows: false,
-        infinite: true,
-        speed: 5000,
-        slidesToShow: 5,
-        slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 0,
-        swipeToSlide: true,
-        focusOnSelect: true,
+        dots: false,
+        draggable: !dragging,
+        infinite: true,
+        onMouseDown: handleDragStart,
+        onMouseLeave: handleDragEnd,
+        onMouseUp: handleDragEnd,
+        onSwipe: handleDragEnd,
+        onTouchEnd: handleDragEnd,
+        onTouchStart: handleDragStart,
+        ref: sliderRef,
+        slidesToScroll: 1,
+        slidesToShow: 5,
+        speed: 5000,
+        swipe: !dragging,
     };
 
     return (
@@ -49,28 +69,32 @@ const WorkCarousel = () => {
                     <div key={work.name} className="work-card py-5">
                         <div className="row">
                             <div className="col-12">
-                                {work.video != null ?
-                                    <>
-                                        <ReactPlayer
-                                            url={work.video?.publicURL}
-                                            controls={false}
-                                            autoPlay={true}
-                                            playing={true}
-                                            playsInline={true}
-                                            muted={true}
-                                            loop={true}
-                                            width="100%"
-                                            height="auto"
-                                        />
-                                        <div className="overlay" />
-                                    </>
-                                    : <Img fixed={work.img?.childImageSharp?.fixed} />}
+                                <Link className="" to={work.link}>
+                                    {work.video != null ?
+                                        <span className="video-wrapper">
+                                            <ReactPlayer
+                                                className="player-wrapper"
+                                                url={work.video?.publicURL}
+                                                controls={false}
+                                                autoPlay={true}
+                                                playing={true}
+                                                playsInline={true}
+                                                muted={true}
+                                                loop={true}
+                                                width="100%"
+                                                height="auto"
+                                            />
+                                            <div className="overlay"></div>
+                                        </span>
+                                        : <Img fixed={work.img?.childImageSharp?.fixed} />}
+                                </Link>
                             </div>
                             <div className="col-12 work-name">
                                 {work.name}
                             </div>
                         </div>
                     </div>
+
                 ))}
 
             </Slider>
