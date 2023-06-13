7fce45a59a1098c1d57b8e9745d59d721b1af70d
Diogo Yaguas <diogo.yaguas@jumpseller.com>
2023-06-13 23:40:22 +0100

    Mobile version

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,11 @@ import { graphql, useStaticQuery } from "gatsby"
 import Footer from "./footer"
 import Header from "./header"
 import PropTypes from "prop-types"
+import Star from '../assets/icons/common/star.svg';
 
 const Layout = ({ children }) => {
   const [mode, setMode] = useState("light")
+  const [isMobile, setIsMobile] = useState(false);
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -35,15 +37,45 @@ const Layout = ({ children }) => {
     } else setMode(prefersDarkMode ? "dark" : "light");
   }, [])
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    // Initial check
+    setIsMobile(mediaQuery.matches);
+
+    // Listen for changes in the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className={mode === "light" ? "light" : "dark"}>
-      <Header
-        siteTitle={data.site.siteMetadata.title}
-        mode={mode}
-        setMode={saveThemeMode}
-      />
-      <main>{children}</main>
-      <Footer />
+      {!isMobile && <>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          mode={mode}
+          setMode={saveThemeMode}
+        />
+        <main>{children}</main>
+        <Footer />
+      </>
+      }
+      {isMobile &&
+
+        <div className="container under-construction">
+          <h1 className="col-10 mx-auto text-center">
+            Mobile Version Under Construction
+          </h1>
+          <Star />
+        </div>}
     </div>
   )
 }
