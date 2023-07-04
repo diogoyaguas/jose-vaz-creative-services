a4c7d7367ef688cf4a72d766fd2c3a488b897805
Diogo Yaguas <diogo.yaguas@jumpseller.com>
2023-07-04 18:44:10 +0100

    Mobile version

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,11 @@ import { graphql, useStaticQuery } from "gatsby"
 import Footer from "./footer"
 import Header from "./header"
 import PropTypes from "prop-types"
+import Star from '../assets/icons/common/star.svg';
 
 const Layout = ({ children }) => {
   const [mode, setMode] = useState("light")
+  const [isMobile, setIsMobile] = useState(false);
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -32,20 +34,53 @@ const Layout = ({ children }) => {
     if (savedThemeMode !== undefined) {
       setMode(savedThemeMode)
     } else setMode(prefersDarkMode ? "dark" : "light");
+
+    if (window.innerWidth < 1024) setIsMobile(true);
+    else setIsMobile(false);
   }, [])
+
+  useEffect(() => {
+    // Update screen size whenever the window is resized
+    const handleResize = () => {
+      if (window.innerWidth < 1024) setIsMobile(true);
+      else setIsMobile(false);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className={mode === "light" ? "light" : "dark"}>
-
-      <Header
-        siteTitle={data.site.siteMetadata.title}
-        mode={mode}
-        setMode={saveThemeMode}
-      />
-      <main>{children}</main>
-      <Footer />
-
-
+      {!isMobile && <>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          mode={mode}
+          setMode={saveThemeMode}
+        />
+        <main>{children}</main>
+        <Footer />
+      </>
+      }
+      {isMobile && <>
+        <div className="mobile-banner container d-flex align-items-center justify-content-center">
+          <div className="row">
+            <div className="col-12 text-center title">
+              MOBILE SITE COMING SOON
+            </div>
+            <div className="col-12 text-center subtile">
+              AVAILABLE ON DESKTOP
+            </div>
+            <div className="col-12 text-center logo mt-2">
+              <Star />
+            </div>
+          </div>
+        </div>
+      </>}
     </div>
   )
 }
