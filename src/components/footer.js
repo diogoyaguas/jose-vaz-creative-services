aa0c5b759e866f148d23578f91dd841799d895f0
Diogo Yaguas <diogo.yaguas@eposnow.com>
2026-02-04 22:36:40 +0000

    feat: new footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,44 +1,55 @@
-import { faBehance, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Footer = () => {
     const date = new Date();
     let year = date.getFullYear();
 
     return (<footer>
-        <div className='container'>
+        <div className='container m-5'>
             <div className="row">
-                <div className="col-12 rights">
-                    @{year} - José Vaz All Rights Reserved
+                <div className='col-6 p-0'>
+                    <StaticImage src={"../assets/images/common/logo.png"} alt="Creative Vaz" />
                 </div>
-                <a className="col-12 pt-2" href="mailto:zeoliveriavaz@gmail.com" target="_blank" rel="noreferrer">
-                    zeoliveiravaz@gmail.com
-                </a>
-                <a className="col-6" href="tel:+351910393289">
-                    +351 910 393 289
-                </a>
-                <div className="col-6 social-media" >
-                    <div>
-                        <a href="https://www.linkedin.com/in/jos%C3%A9-vaz-45144a1b9/" target="_blank" rel="noreferrer" title="Linkedin">
-                            <FontAwesomeIcon icon={faLinkedin} />
-                        </a>
-                    </div>
-                    <div>
-                        <a href="https://www.behance.net/zeoliveira2936" target="_blank" rel="noreferrer" title="Behance">
-                            <FontAwesomeIcon icon={faBehance} />
-                        </a>
-                    </div>
-                    <div>
-                        <a href="https://www.instagram.com/sr.josevaz/" target="_blank" rel="noreferrer" title="Instagram">
-                            <FontAwesomeIcon icon={faInstagram} />
-                        </a>
+                <div className='col-1 p-0'></div>
+                <div className='col-5 p-0'>
+                    <div className="row h-100">
+                        <div className='col-5 d-flex flex-column h-100 p-0'>
+                            <div>
+                                <a className='d-block p-0' href="mailto:zeoliveriavaz@gmail.com" target="_blank" rel="noreferrer">
+                                    zeoliveiravaz@gmail.com
+                                </a>
+                                <a className='d-block p-0' href="tel:+351910393289">
+                                    +351 910 393 289
+                                </a>
+                            </div>
+
+                            <div className='mt-auto'>
+                                @{year} - José Vaz
+                            </div>
+                        </div>
+                        <div className='col-3 text-center p-0'>
+                            <div className='col-12'>
+                                <a href="https://www.linkedin.com/in/jos%C3%A9-vaz-45144a1b9/" target="_blank" rel="noreferrer" title="Linkedin">
+                                    LinkedIn
+                                </a>
+                            </div>
+                            <div className='col-12'>
+                                <a href="https://www.behance.net/zeoliveira2936" target="_blank" rel="noreferrer" title="Behance">
+                                    Behance
+                                </a>
+                            </div>
+                        </div>
+                        <div className='col-4 text-center p-0'>
+                            <a href="/José-Vaz-CV.pdf" download>
+                                download CV
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>)
+    </footer >)
 }
 
 export default Footer
