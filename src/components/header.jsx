5ea9828cf3bdcaf3f4026b4ea92a7ab7df3b79b0
Diogo Yaguas <diogo.yaguas@eposnow.com>
2026-02-07 09:17:21 +0000

    feat: make header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,42 +1,62 @@
+import React, { useState } from "react"
+
 import { Link } from "gatsby"
 import Logo from "../assets/icons/common/creative_vaz.svg"
 import PropTypes from "prop-types"
-import React from "react"
 
 const Header = () => {
-	return (
-		<nav className="navbar navbar-expand-lg sticky-top">
-			<div className="container">
+  const [open, setOpen] = useState(false)
 
-				<div className="navbar-brand logo logo-svg">
-					<Link to="/projetos" className="nav-link">
-						<Logo />
-					</Link>
-				</div>
+  const closeMenu = () => setOpen(false)
+  const toggleMenu = () => setOpen((v) => !v)
 
-				<div className="ms-auto d-flex gap-5">
-					<Link to="/projetos" className="nav-link">
-						Projetos
-					</Link>
-					<Link to="/contacto" className="nav-link">
-						Contacto
-					</Link>
-					<Link to="/sobre" className="nav-link">
-						Sobre
-					</Link>
-				</div>
+  return (
+    <nav className="navbar navbar-expand-md sticky-top">
+      <div className="container">
+        <div className="navbar-brand logo logo-svg">
+          <Link to="/projetos" className="nav-link" onClick={closeMenu}>
+            <Logo />
+          </Link>
+        </div>
 
-			</div>
-		</nav>
-	)
+        <button
+          className="navbar-toggler"
+          type="button"
+          aria-controls="mainNavbar"
+          aria-expanded={open}
+          aria-label="Toggle navigation"
+          onClick={toggleMenu}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div
+          id="mainNavbar"
+          className={`collapse navbar-collapse ${open ? "show" : ""}`}
+        >
+          <div className="ms-auto d-flex gap-5 navbar-links">
+            <Link to="/projetos" className="nav-link" onClick={closeMenu}>
+              Projetos
+            </Link>
+            <Link to="/contacto" className="nav-link" onClick={closeMenu}>
+              Contacto
+            </Link>
+            <Link to="/sobre" className="nav-link" onClick={closeMenu}>
+              Sobre
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 Header.propTypes = {
-	siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-	siteTitle: ``,
+  siteTitle: ``,
 }
 
 export default Header
