73b11827421de7b1235851b35fdc1575175efd94
Diogo Yaguas <diogo.yaguas@eposnow.com>
2026-02-08 12:57:12 +0000

    feat: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: false,
+        es6: true,
+    },
+    globals: {
+        process: "readonly",
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+    },
+    plugins: ["react", "react-hooks", "unused-imports", "import"],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    settings: {
+        react: { version: "detect" },
+    },
+    ignorePatterns: ["public", ".cache", "node_modules"],
+    rules: {
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+    overrides: [
+        {
+            files: [
+                "gatsby-*.js",
+                "gatsby-config.js",
+                "gatsby-node.js",
+                "gatsby-browser.js",
+                "gatsby-ssr.js",
+                "netlify/functions/**/*.js",
+            ],
+            env: { node: true, browser: false, es6: true },
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: "module",
+            },
+        },
+    ],
+}
