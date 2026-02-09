405415634d2d7af8f053df5bf672d535e591781b
Diogo Yaguas <diogo.yaguas@eposnow.com>
2026-02-09 20:16:11 +0000

    feat: add theme transactions

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,20 +1,29 @@
 const React = require("react")
 
-function setInitialTheme() {
-    const code = `
+function setInitialThemeAndEnableTransitionsLater() {
+  const code = `
 (function() {
   try {
     var stored = localStorage.getItem("theme");
-    var theme = stored === "light" || stored === "dark"
+    var theme = (stored === "light" || stored === "dark")
       ? stored
       : (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
     document.documentElement.setAttribute("data-theme", theme);
+
+    requestAnimationFrame(function () {
+      requestAnimationFrame(function () {
+        document.documentElement.classList.remove("no-theme-transition");
+      });
+    });
   } catch (e) {}
 })();
 `
-    return <script dangerouslySetInnerHTML={{ __html: code }} />
+  return <script dangerouslySetInnerHTML={{ __html: code }} />
 }
 
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-    setPreBodyComponents([setInitialTheme()])
+exports.onRenderBody = ({ setPreBodyComponents, setHtmlAttributes }) => {
+  setHtmlAttributes({ className: "no-theme-transition" })
+
+  setPreBodyComponents([setInitialThemeAndEnableTransitionsLater()])
 }
