03985c12aa728ffb6441cc7c0fe30aa87132d25e
Diogo Yaguas <diogo.yaguas@eposnow.com>
2026-02-15 12:30:45 +0000

    feat: start page in top of page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,41 @@
+/* global window */
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./src/styles/index.scss"
+
+const forceScrollTop = () => {
+  if (typeof window === "undefined") return
+
+  const root = window.document?.documentElement
+  const body = window.document?.body
+
+  if (root) root.scrollTop = 0
+  if (body) body.scrollTop = 0
+  window.scrollTo(0, 0)
+}
+
+export const onInitialClientRender = () => {
+  if (typeof window === "undefined") return
+  window.history.scrollRestoration = "manual"
+  forceScrollTop()
+}
+
+export const shouldUpdateScroll = () => false
+
+export const onPreRouteUpdate = () => {
+  forceScrollTop()
+}
+
+export const onRouteUpdate = () => {
+  forceScrollTop()
+
+  if (typeof window === "undefined") return
+
+  window.requestAnimationFrame(() => {
+    forceScrollTop()
+    window.requestAnimationFrame(() => {
+      forceScrollTop()
+    })
+  })
+}
